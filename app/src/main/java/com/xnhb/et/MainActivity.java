8d0e0362b261bc1425a4d67f500f40394e63a12f
oneway2030 <ww4060586>
2018-09-10 10:37:24 +0800

    上传配置

--- app/src/main/java/com/xnhb/et/MainActivity.java
+++ app/src/main/java/com/xnhb/et/MainActivity.java
@@ -1,9 +1,21 @@
 package com.xnhb.et;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static void launch(Context context) {
+        Intent intent = new Intent();
+        intent.setClass(context, MainActivity.class);
+        if (!(context instanceof Activity)) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/xnhb/et/ui/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.xnhb.et.ui;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.oneway.ui.base.ac.BaseStatusBarActivity;
+import com.xnhb.et.MainActivity;
+import com.xnhb.et.R;
+
+/**
+ * 作者 oneway on 2018/9/6
+ * 描述:
+ * 参考链接:
+ */
+public class SplashActivity extends BaseStatusBarActivity {
+
+    @Override
+    protected void setStatusBar() {
+
+    }
+
+    @Override
+    protected int bindView(Bundle savedInstanceState) {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+        adTask();
+    }
+
+    private void adTask() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toNext();
+            }
+        }, 1000);
+    }
+
+    private void toNext() {
+        MainActivity.launch(this);
+        finish();
+    }
+}
